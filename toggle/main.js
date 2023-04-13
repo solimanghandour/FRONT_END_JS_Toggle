ca5973b06b5d2ff0594f3786a7356f14aa462881
solimanghandour <[200~solimanghandour21@gmail.com>
2023-04-13 05:02:44 +0200

    upload_toggele

---
+++ toggle/main.js
@@ -0,0 +1,12 @@
+var x=1
+function toggel(){
+    if(x==1){
+        document.getElementById('div2').style.display='none'
+        x=2
+
+    }
+    else{ document.getElementById('div2').style.display='block'
+    x=1
+
+    }
+}
